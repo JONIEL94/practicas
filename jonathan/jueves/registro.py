7d6667ff1c062b5ae49c77353be90d495f20f198
JONIEL94 <gomez_arg@outlook.es>
2018-11-27 21:15:35 -0300

    verificar 3 opcion menu

--- jonathan/jueves/registro.py
+++ jonathan/jueves/registro.py
@@ -1,26 +1,36 @@
 def agregarAlumnos(alumnos):
 	alumno = {}
 	
-	otro = "s"
+	otro = "si"
 
-	while otro == "s":
+	while otro == "si":
 		alumno['Nombre'] = input('Ingrese el nombre: ')
 		alumno['Apellido'] = input('Ingrese el Apellido: ')
 		alumno['DNI'] = int(input('Ingrese el DNI: '))
 		alumno['Direccion'] = input('Ingrese direccion:')
 		alumno['Telefono'] = input('Ingrese telefono: ')
 		alumno['Email'] = input('Ingrese direccion de email:')
-		alumno['Fecha de nacimiento'] = input('Ingrese fecha de nac: ')
+		alumno['Fecha de nacimiento'] = input('Ingrese fecha de nacimiento: ')
 
 		alumnos.append(alumno)
 
-		otro = input("Agregar otro alumno? (s/n)")
+		otro = input("Agregar otro alumno? (si/no) : ")
 
 	return alumnos
 
 def listarAlumnos(alumnos):
 	for alumno in alumnos:
 		print(alumno)
+
+def borrarAlumno(alumnos):
+	apellido = input("Ingrese apellido de alumno a borrar: ")
+	if (apellido == alumnos):
+
+		alumnos.remove(alumno)
+
+	otro = input("Borrar otro alumno ? (si/no) : ")
+	
+	return alumnos
 #main
 alumnos = []
 
@@ -29,6 +39,7 @@ opcion = -1
 while opcion != 0:
 	print("1 Agregar alumnos")
 	print("2 Listar alumnos")
+	print("3 Borrar alumno por nombre: ")
 	print("0 Salir")
 
 	opcion = int(input('Seleccione una opcion: '))
@@ -37,3 +48,5 @@ while opcion != 0:
 		alumnos = agregarAlumnos(alumnos)
 	elif opcion == 2:
 		listarAlumnos(alumnos)
+	elif opcion == 3:
+		borrarAlumno(alumnos)
--- jonathan/jueves/registroa.py
+++
@@ -1,3 +0,0 @@
-def menu():
-option = int(input("[1] Ver alumnos [2] Agregar alumnos [3] Borrar alumno ")
-
